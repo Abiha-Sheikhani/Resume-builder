2f54828a2a898617653a5e5c9d8bc97e18f01fbf
Abiha-Sheikhani <abihajawed345@gmail.com>
2026-01-31 15:20:57 +0500

    work

--- src/Pages/ResumeDetail.jsx
+++ src/Pages/ResumeDetail.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import supabase from "../config";
 import Swal from "sweetalert2";
-import "../index.css"
+import "../ResumeDetail.css"
 
 
 const ResumeDetail = () => {
